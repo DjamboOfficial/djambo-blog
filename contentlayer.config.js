986743cafa15f96391e4e794c1cd08a4b1bb9aa2
Edoardo Giambuzzi <edoardo.giambuzzi@gmail.com>
2024-05-30 16:18:57 +0200

    add contentlayer

---
+++ contentlayer.config.js
@@ -0,0 +1,47 @@
+import { makeSource, defineDocumentType } from "@contentlayer/source-files";
+
+// Define the document type for the blog posts
+const Blog = defineDocumentType(() => ({
+  name: "Blog",
+  filePathPattern: "**/*.mdx",
+  contentType: "mdx",
+  fields: {
+    title: {
+      type: "string",
+      required: true,
+    },
+    publishedAt: {
+      type: "date",
+      required: true,
+    },
+    updatedAt: {
+      type: "date",
+      required: true,
+    },
+    description: {
+      type: "string",
+      required: true,
+    },
+    image: {
+      type: string,
+      required: true,
+    },
+    isPublished: {
+      type: "boolean",
+      default: true,
+    },
+    author: {
+      type: "string",
+      required: true,
+    },
+    tags: {
+      type: "list",
+      of: { type: "string" },
+    },
+  },
+}));
+
+export default makeSource({
+  contentDirPath: "content",
+  documentTypes: [Blog],
+});
